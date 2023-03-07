3029a832ab85cceb67f2245b327227ab86314ba9
franrodriguez1993 <francojmprogramacion@gmail.com>
2023-03-07 08:52:04 -0300

    project and contact json created

---
+++ src/routes/HomeRoute.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../css/HomeRoute/HomeRoute.css";
+const HomeRoute = () => {
+  return (
+    <div className="homeroute-container">
+      <div className="homeroute-container_title">
+        <h1 className="homeroute-title">Nerd Biber</h1>
+        <h3 className="homeroute-subtitle">
+          We design your website. We build your experience.
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default HomeRoute;
